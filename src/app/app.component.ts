98a631a51313ad6934e5ff91244e17a67cb21953
Patrick Hendji <hendjipatrick@yahoo.fr>
2026-07-17 06:08:46 +0200

    [STEP1] - Display floating rate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
+import { FloatingRateService } from './services/floating-rate.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  private readonly _floatingRateService = inject(FloatingRateService);
+
+  protected currentRate: Signal<number> = this._floatingRateService.currentRate;
 }
